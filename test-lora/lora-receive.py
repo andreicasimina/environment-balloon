cac83bec7b13af2d8fece0a617e5841bda37e2a3
andreicasimina <andreicasimina1208@gmail.com>
2024-08-07 14:43:55 +0900

    Add: LoRa testing files

---
+++ test-lora/lora-receive.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# LoRa Receive Program
+# Last updated: 2024.08.07
+# Created by: Andrei Casimina
+
+import serial
+import time
+import datetime
+import subprocess
+import os
+import csv
+from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_EVEN #四捨五入用
+
+subprocess.call(["sudo", "systemctl", "stop", "serial-getty@ttyS0.service"])
+subprocess.call(["sudo", "systemctl", "disable", "serial-getty@ttyS0.service"])
+
+con = serial.Serial(            #LoRa用の設定
+    port = "/dev/ttyS0",        #Raspiシリアル通信を用いる
+    baudrate = 115200,              #baudレート
+    parity = serial.PARITY_NONE,    #パリティ
+    bytesize = serial.EIGHTBITS,    #データのビット数
+    stopbits  = serial.STOPBITS_ONE,    #ストップビット数
+    rtscts = 0,                     #RTS/CTSフロー制御
+)
+
+while True:
+    received_message = con.readline().decode() #1行ごとに読み込み、処理を繰り返す
+
+    if received_message != None:
+        print(received_message)
+
+        reply = "Got your message!\r\n"        
+
+        con.write(reply.encode())
+    else:
+        print("No messages received!")
+
+
+    con.flushInput()
+    con.flushOutput()
---
+++ test-lora/lora-send.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# LoRa Send Program
+# Last updated: 2024.08.07
+# Created by: Andrei Casimina
+
+import serial
+import datetime
+import time
+import subprocess
+
+subprocess.call(["sudo","systemctl","stop","serial-getty@ttyS0.service"])
+subprocess.call(["sudo","systemctl","disable","serial-getty@ttyS0.service"])
+
+con = serial.Serial(               #LoRa用の設定
+        port = "/dev/ttyS0",           #RasPiシリアル通信を用いる
+        baudrate = 115200,                 #baudレート
+        parity = serial.PARITY_NONE,     #パリティ
+        bytesize = serial.EIGHTBITS,     #データのビット数
+        stopbits = serial.STOPBITS_ONE,  #ストップビット数
+        timeout = 5,                  #タイムアウト値
+        xonxoff = 0,                     #ソフトウェアフロー制御
+        rtscts = 0,                      #RTS/CTSフロー制御
+    )
+
+time.sleep(1)
+
+while True:
+    message = "Hello, I am transmitter-a!\r\n"        
+
+    con.write(message.encode())
+
+    reply_message = con.readline()
+    reply_message = reply_message.decode()
+
+    print(reply_message)
+        
+    con.flushInput()
+    con.flushOutput()
